8fc2ed9edb9de924e7b895b18cc1cdafc1464fa5
Phan-Ngoc-Hanh-Nhi <155942839+nhiph4303@users.noreply.github.com>
2025-01-17 19:15:33 +0700

    Update EI2122Q1ADAM1.java

--- EI2122Q1ADAM1.java
+++ EI2122Q1ADAM1.java
@@ -1,3 +1,90 @@
+import java.io.*;
+import java.util.*;
+
 public class EI2122Q1ADAM1 {
-    
+    static InputReader sc = new InputReader(System.in);
+
+    public static void main(String[] args) {
+        
+    }
+
+    static class InputReader {
+
+        StringTokenizer tokenizer;
+
+        BufferedReader reader;
+
+        String token;
+
+        String temp;
+
+        public InputReader(InputStream stream) {
+
+            tokenizer = null;
+
+            reader = new BufferedReader(new InputStreamReader(stream));
+
+        }
+
+        public InputReader(FileInputStream stream) {
+
+            tokenizer = null;
+
+            reader = new BufferedReader(new InputStreamReader(stream));
+
+        }
+
+        public String nextLine() throws IOException {
+
+            return reader.readLine();
+
+        }
+
+        public String next() {
+
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+
+                try {
+
+                    if (temp != null) {
+
+                        tokenizer = new StringTokenizer(temp);
+
+                        temp = null;
+
+                    } else {
+
+                        tokenizer = new StringTokenizer(reader.readLine());
+
+                    }
+
+                } catch (IOException e) {
+
+                }
+
+            }
+
+            return tokenizer.nextToken();
+
+        }
+
+        public double nextDouble() {
+
+            return Double.parseDouble(next());
+
+        }
+
+        public int nextInt() {
+
+            return Integer.parseInt(next());
+
+        }
+
+        public long nextLong() {
+
+            return Long.parseLong(next());
+
+        }
+
+    }
 }
