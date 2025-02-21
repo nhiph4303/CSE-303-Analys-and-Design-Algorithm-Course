230b1bbed3274794a217c5abc4f0c4c2c127e4e0
Phan-Ngoc-Hanh-Nhi <155942839+nhiph4303@users.noreply.github.com>
2025-02-21 11:36:13 +0700

    Update EIUQBHV.java

--- EIUQBHV.java
+++ EIUQBHV.java
@@ -4,9 +4,47 @@ import java.util.*;
 public class EIUQBHV {
     static InputReader sc = new InputReader(System.in);
     static StringBuilder sb = new StringBuilder();
+    static int count;
+    static char[] arr;
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        arr = sc.next().toCharArray();
+        Arrays.sort(arr);
+        sb.append(String.valueOf(arr)).append("\n");
+        count++;
+        while (nextPermutation()) {
+            sb.append(String.valueOf(arr)).append("\n");
+            count++;
+        }
+        System.out.println(count + "\n" + sb);
+    }
+
+    static boolean nextPermutation() {
+        int i = arr.length - 2;
+        while (i >= 0 && arr[i] >= arr[i + 1])
+            i--;
+        if (i < 0) {
+            return false;
+        }
+        int j = arr.length - 1;
+        while (arr[j] <= arr[i]) {
+            j--;
+        }
+        swap(i, j);
+        reverse(i + 1, arr.length - 1);
+        return true;
+    }
+
+    static void reverse(int start, int end) {
+        while (start < end) {
+            swap(start++, end--);
+        }
+    }
+
+    static void swap(int i, int j) {
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
     static class InputReader {
