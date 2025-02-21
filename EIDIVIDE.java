94a47cd6c5f375e5ab279427f2691c55a03c0d4d
Phan-Ngoc-Hanh-Nhi <155942839+nhiph4303@users.noreply.github.com>
2025-02-21 11:50:27 +0700

    Update EIDIVIDE.java

--- EIDIVIDE.java
+++ EIDIVIDE.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class EIDIVIDE {
     static StringBuilder sb = new StringBuilder();
-    static InputReader sc= new InputReader(System.in);
+    static InputReader sc = new InputReader(System.in);
 
     public static void main(String[] args) throws IOException {
         long n = sc.nextLong();
@@ -35,130 +35,82 @@ public class EIDIVIDE {
     }
 
     static class InputReader {
-        private byte[] inbuf = new byte[2 << 23];
-        public int lenbuf = 0, ptrbuf = 0;
-        public InputStream is;
 
-        public InputReader(InputStream stream) throws IOException {
+        StringTokenizer tokenizer;
 
-            inbuf = new byte[2 << 23];
-            lenbuf = 0;
-            ptrbuf = 0;
-            is = System.in;
-            lenbuf = is.read(inbuf);
+        BufferedReader reader;
+
+        String token;
+
+        String temp;
+
+        public InputReader(InputStream stream) {
+
+            tokenizer = null;
+
+            reader = new BufferedReader(new InputStreamReader(stream));
+
         }
 
-        public InputReader(FileInputStream stream) throws IOException {
-            inbuf = new byte[2 << 23];
-            lenbuf = 0;
-            ptrbuf = 0;
-            is = stream;
-            lenbuf = is.read(inbuf);
-        }
+        public InputReader(FileInputStream stream) {
 
-        public boolean hasNext() throws IOException {
-            if (skip() >= 0) {
-                ptrbuf--;
-                return true;
-            }
-            return false;
+            tokenizer = null;
+
+            reader = new BufferedReader(new InputStreamReader(stream));
+
         }
 
         public String nextLine() throws IOException {
-            int b = skip();
-            StringBuilder sb = new StringBuilder();
-            while (!isSpaceChar(b) && b != ' ') { // when nextLine, ()
-                sb.appendCodePoint(b);
-                b = readByte();
-            }
-            return sb.toString();
+
+            return reader.readLine();
+
         }
 
         public String next() {
-            int b = skip();
-            StringBuilder sb = new StringBuilder();
-            while (!(isSpaceChar(b))) { // when nextLine, (isSpaceChar(b) && b
-                                        // != ' ')
-                sb.appendCodePoint(b);
-                b = readByte();
-            }
-            return sb.toString();
-        }
 
-        private int readByte() {
-            if (lenbuf == -1)
-                throw new InputMismatchException();
-            if (ptrbuf >= lenbuf) {
-                ptrbuf = 0;
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+
                 try {
-                    lenbuf = is.read(inbuf);
+
+                    if (temp != null) {
+
+                        tokenizer = new StringTokenizer(temp);
+
+                        temp = null;
+
+                    } else {
+
+                        tokenizer = new StringTokenizer(reader.readLine());
+
+                    }
+
                 } catch (IOException e) {
-                    throw new InputMismatchException();
+
                 }
-                if (lenbuf <= 0)
-                    return -1;
+
             }
-            return inbuf[ptrbuf++];
+
+            return tokenizer.nextToken();
+
         }
 
-        private boolean isSpaceChar(int c) {
-            return !(c >= 33 && c <= 126);
-        }
+        public double nextDouble() {
 
-        private double nextDouble() {
             return Double.parseDouble(next());
-        }
 
-        public Character nextChar() {
-            return skip() >= 0 ? (char) skip() : null;
-        }
-
-        private int skip() {
-            int b;
-            while ((b = readByte()) != -1 && isSpaceChar(b))
-                ;
-            return b;
         }
 
         public int nextInt() {
-            int num = 0, b;
-            boolean minus = false;
-            while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
-                ;
-            if (b == '-') {
-                minus = true;
-                b = readByte();
-            }
 
-            while (true) {
-                if (b >= '0' && b <= '9') {
-                    num = num * 10 + (b - '0');
-                } else {
-                    return minus ? -num : num;
-                }
-                b = readByte();
-            }
+            return Integer.parseInt(next());
+
         }
 
         public long nextLong() {
-            long num = 0;
-            int b;
-            boolean minus = false;
-            while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
-                ;
-            if (b == '-') {
-                minus = true;
-                b = readByte();
-            }
 
-            while (true) {
-                if (b >= '0' && b <= '9') {
-                    num = num * 10 + (b - '0');
-                } else {
-                    return minus ? -num : num;
-                }
-                b = readByte();
-            }
+            return Long.parseLong(next());
+
         }
+
     }
 }
