543a6fd16210d9042d26a50965cf689a5243bf46
Phan-Ngoc-Hanh-Nhi <155942839+nhiph4303@users.noreply.github.com>
2025-03-24 22:10:36 +0700

    Update EIMIN.java

---
+++ Final/EIMIN.java
@@ -0,0 +1,25 @@
+package Final;
+
+import java.util.*;
+
+public class EIMIN {
+    static Scanner sc = new Scanner(System.in);
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        TreeSet<Long> tree = new TreeSet<>();
+        for (int i = 0; i < 0; i++) {
+            tree.add(sc.nextLong());
+        }
+
+        ArrayList<Long> list = new ArrayList<>();
+        long prev = 0L;
+
+        for (Long num : tree) {
+            
+        }
+    }
+}
